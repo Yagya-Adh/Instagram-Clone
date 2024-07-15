364f341fdacad1f975ee797297e99586165a4258
Yagya Adhikari <yagyaadhikari02@gmail.com>
2024-07-15 12:44:54 +0545

    login form is completed

--- instagram/src/Pages/Login.jsx
+++ instagram/src/Pages/Login.jsx
@@ -3,10 +3,22 @@ import LoginForm from "../components/login/LoginForm";
 
 const Login = () => {
   return (
-    <div className="relative flex justify-center items-center h-screen bg-white dark:bg-white">
+    <div className="relative flex flex-col justify-center items-center h-screen bg-white dark:bg-white">
       <LoginForm />
+      <SignUpCard />
     </div>
   );
 };
 
 export default Login;
+
+const SignUpCard = () => {
+  return (
+    <div className=" p-3 my-3 relative shadow-lg flex justify-center items-center border text-center border-gray-400 w-1/4 h-auto bg-white dark:bg-white">
+      <h2 className="text-sm mx-1">Don't have an account?</h2>
+      <a href="" className="text-blue-500 font-bold ">
+        Signup
+      </a>
+    </div>
+  );
+};
--- instagram/src/components/login/LoginForm.jsx
+++ instagram/src/components/login/LoginForm.jsx
@@ -13,33 +13,35 @@ const LoginForm = () => {
     setShowPassword((prev) => !prev);
   };
   return (
-    <div className="relative shadow-lg flex flex-col justify- items-center border border-gray-400 w-1/4 h-2/5 bg-white dark:bg-white">
-      <img src={Insta} alt="" />
+    <div className="p-4 relative shadow-lg flex flex-col justify-center items-center border text-center border-gray-400 w-1/4 h-auto bg-white dark:bg-white">
+      <img src={Insta} alt="logo" />
 
-      <div className="mb-3">
-        <input
-          type="text"
-          className="text-sm font-light bg-white appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 "
-          placeholder="Phone number,username, or email"
-        />
-      </div>
+      <div className="flex flex-col -mt-6 mb-7">
+        <div className="mb-1">
+          <input
+            type="text"
+            className="text-sm font-light bg-white appearance-none border border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 "
+            placeholder="Phone number,username, or email"
+          />
+        </div>
 
-      <div className="mb-3 relative">
-        <input
-          type={showPassword ? "text" : "password"}
-          className="text-sm font-light bg-white appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 "
-          placeholder="Password"
-          onChange={handleChange}
-        />
-        {password && (
-          <button
-            onClick={togglePasswordVisiblity}
-            type="button"
-            className="absolute right-2 top-2 text-sky-400"
-          >
-            {showPassword ? "Hide" : "show"}
-          </button>
-        )}
+        <div className="mb-3 relative">
+          <input
+            type={showPassword ? "text" : "password"}
+            className="text-sm font-light bg-white appearance-none border border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 "
+            placeholder="Password"
+            onChange={handleChange}
+          />
+          {password && (
+            <button
+              onClick={togglePasswordVisiblity}
+              type="button"
+              className="absolute right-2 top-2 text-sky-400"
+            >
+              {showPassword ? "Hide" : "show"}
+            </button>
+          )}
+        </div>
       </div>
 
       <button className="px-2 py-1 rounded bg-sky-500 text-white font-bold text-center">
@@ -47,6 +49,13 @@ const LoginForm = () => {
       </button>
       <div className="flex">
         <h1 className="text-center font-bold text-gray-500">OR</h1>
+      </div>
+
+      <div className="mb-3 p-1">
+        <h3 className="mb-0 font-extrabold font-serif text-blue-900">
+          Log in with Facebook
+        </h3>
+        <span className="font-serif text-blue-950">Forgot password?</span>
       </div>
     </div>
   );
