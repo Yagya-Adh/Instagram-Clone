0513e06406b3b6ff315b005b942efedbc2141aea
Yagya Adhikari <yagyaadhikari02@gmail.com>
2024-07-15 12:30:17 +0545

    login page

--- instagram/src/Pages/Login.jsx
+++ instagram/src/Pages/Login.jsx
@@ -3,7 +3,7 @@ import LoginForm from "../components/login/LoginForm";
 
 const Login = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="relative flex justify-center items-center h-screen bg-white dark:bg-white">
       <LoginForm />
     </div>
   );
--- instagram/src/components/login/LoginForm.jsx
+++ instagram/src/components/login/LoginForm.jsx
@@ -1,24 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import Insta from "../../assets/imgs/instagram.jpeg";
 const LoginForm = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const [password, setPassword] = useState("");
+
+  const handleChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const togglePasswordVisiblity = () => {
+    setShowPassword((prev) => !prev);
+  };
   return (
-    <div className="flex flex-col justify- items-center border border-gray-400 w-1/4 h-2/5">
+    <div className="relative shadow-lg flex flex-col justify- items-center border border-gray-400 w-1/4 h-2/5 bg-white dark:bg-white">
       <img src={Insta} alt="" />
 
-      <div className=" ">
+      <div className="mb-3">
         <input
           type="text"
-          className="border px-2 py-1 "
+          className="text-sm font-light bg-white appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 "
           placeholder="Phone number,username, or email"
         />
       </div>
 
-      <div className="">
+      <div className="mb-3 relative">
         <input
-          type="text"
-          className="border px-2 py-1 "
+          type={showPassword ? "text" : "password"}
+          className="text-sm font-light bg-white appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 "
           placeholder="Password"
+          onChange={handleChange}
         />
+        {password && (
+          <button
+            onClick={togglePasswordVisiblity}
+            type="button"
+            className="absolute right-2 top-2 text-sky-400"
+          >
+            {showPassword ? "Hide" : "show"}
+          </button>
+        )}
       </div>
 
       <button className="px-2 py-1 rounded bg-sky-500 text-white font-bold text-center">
